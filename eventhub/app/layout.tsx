142c776dd66b686bfedfe4ccfbfb1b886390efc9
Your Name <jish261203@gmail.comgit config --global user.name Jishnay>
2024-01-20 02:00:33 +0530

    Implemented database setup and basic home page

---
+++ eventhub/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+
+const poppins = Poppins({
+	subsets: ["latin"],
+	weight: ["400", "500", "600", "700"],
+	variable: "--font-poppins ",
+});
+
+export const metadata: Metadata = {
+	title: "EVENT AQUARIUM",
+	description: "EventAquarium is a platform for event hosting and managing.",
+	icons: {
+		icon: "/assets/images/logo1.svg",
+	},
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				<body className={poppins.variable}>{children}</body>
+			</html>
+		</ClerkProvider>
+	);
+}
